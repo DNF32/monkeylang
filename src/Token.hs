3e33f85e4ef0774062fe99286693e94fb94548e7
DNF32 <137726307+DNF32@users.noreply.github.com>
2025-12-27 00:10:01 +0000

    initial

---
+++ src/Token.hs
@@ -0,0 +1,60 @@
+module Token where
+
+data TokenType
+  = Illegal
+  | EOF
+  | Identifier String
+  | IntLiteral Int
+  | StringLiteral String
+  | Assign
+  | Plus
+  | Minus
+  | Bang
+  | Asterisk
+  | Slash
+  | Equal
+  | NotEqual
+  | LessThan
+  | GreaterThan
+  | Comma
+  | Semicolon
+  | Colon
+  | LeftParen
+  | RightParen
+  | LeftBrace
+  | RightBrace
+  | LeftBracket
+  | RightBracket
+  | Function
+  | Let
+  | TrueLit
+  | FalseLit
+  | If
+  | Else
+  | Return
+  deriving (Show, Eq)
+
+data Token = Token
+  { tokenType :: TokenType,
+    tokenPosition :: Position
+  }
+  deriving (Show, Eq)
+
+data Position = Position
+  { line :: Int,
+    column :: Int
+  }
+  deriving (Show, Eq)
+
+data LexError = LexError
+  { errorMsg :: String,
+    errorPosition :: Position
+  }
+
+data LexerState = LexerState
+  { getInput :: String,
+    currentPosition :: Position
+  }
+
+nextToken :: LexerState -> Either LexError (Token, LexerState)
+nextToken = undefined
