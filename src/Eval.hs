c4892dfef5be9385663d385a067343b46c1e1e8f
DNF32 <137726307+DNF32@users.noreply.github.com>
2026-02-01 23:50:11 +0000

    started the eval using the StateT monad

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,13 +1,52 @@
 module Eval where
 
-import Ast
+import Ast (Expression (..), Statement (..))
+import Control.Monad.State
+import Data.Map qualified as Map
 
-type Enviroment = ()
+emptyMap :: Map.Map k v
+emptyMap = Map.empty
 
-type Object = ()
+data Object
+  = IntO Integer
+  | BoolO Bool
+  | FloatO Float
+  | StringO String
+  | ArrayO [Object]
+  deriving (Show, Eq)
 
-evalStatement :: Statement -> Enviroment -> Object
-evalStatement = undefined
+type Enviroment = Map.Map String Object
 
-evalExpression :: Expression -> Enviroment -> Enviroment
+type Eval a = State Enviroment a
+
+hasIdent :: String -> Eval Bool
+hasIdent name = gets (Map.member name)
+
+setObject :: String -> Object -> Eval ()
+setObject name obj = modify (Map.insert name obj)
+
+getObject :: String -> Eval (Maybe Object)
+getObject name = gets (Map.lookup name)
+
+newEnv :: Enviroment
+newEnv = Map.empty
+
+initialEnv :: Eval ()
+initialEnv = put newEnv
+
+evalLetStatement :: Statement -> Eval Object
+evalLetStatement (LetStatement tok (IdentifierLit _ name) value) = do
+  obj <- evalExpression value
+  setObject name obj
+  return obj
+evalLetStatement _ = undefined
+
+evalReturnStatement :: Statement -> Eval Object
+evalReturnStatement (ReturnStatement tok expr) = do
+  evalExpression expr
+
+evalExpressionStatement :: Statement -> Eval Object
+evalExpressionStatement = undefined
+
+evalExpression :: Expression -> Eval Object
 evalExpression = undefined
